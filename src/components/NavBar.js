2b430816cd4d3ed6556745a60e20793be0a33a81
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-07-03 15:34:39 -0400

    adds link router to the NavBAr Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,22 @@
+import { Link } from 'react-router-dom';
+
 function NavBar () {
     return (
-        <></>
+        <nav>
+            <ul>
+                <li>
+            <Link to="/">Home</Link>
+             </li>
+             <li>
+             <Link to="/recipes">Recipes</Link>
+            
+             </li>
+             <li>
+             <Link to="/recipes/new">New Recipe</Link>
+             </li>
+
+            </ul>
+        </nav>
     )
 }
 
