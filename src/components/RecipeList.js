ac45bc1a4cd57f93755479165ec1c836cce944ab
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-07-03 18:05:56 -0400

    adds a recipelist and recipedetails

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,31 @@
-function RecipeList () {
-    return (
-        <></>
-    )
+import React, { useState, useEffect } from 'react';
+
+function RecipeList() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://backend-phase-2-project-2ll9.onrender.com/recipes')
+      .then(response => response.json())
+      .then(data => setRecipes(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const recipeItems = recipes.map(recipe => (
+    <li key={recipe.id}>
+      <a href={`/recipes/${recipe.id}`}>{recipe.title}</a>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.length > 0 ? (
+        <ul>{recipeItems}</ul>
+      ) : (
+        <p>No recipes found.</p>
+      )}
+    </div>
+  );
 }
 
 export default RecipeList;
