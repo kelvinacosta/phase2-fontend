7f5a95d9a88bb4a762c10d3cff21bf2327690f09
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-07-10 20:44:17 -0400

    updates coments to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import "../homecss.css"
 
 function Home() {
   return (
+    
+    //Create a div to store a good looking welcoming title to the app
     <div className="home-container">
       <div className="home-content">
         <h1 className="home-title">Welcome to The Ecuadorian Recipe Form</h1>
